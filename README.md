daeb534d60620c49ea76257afb9e318f7512e96d
Tiago Boeing <contato@tiagoboeing.com>
2020-10-25 02:51:10 -0300

    Create script

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# paste-images-from-clipboard
-Created with CodeSandbox
+# Paste images from clipboard
+
+A JavaScript script to paste images from Clipboard.
+
+Inspired on [Our Code World](http://ourcodeworld.com/articles/read/491/how-to-retrieve-images-from-the-clipboard-with-javascript-in-the-browser) script example.
+
+| Mode     | Implemented |
+| -------- | ----------- |
+| `blob`   | ✅ Yes       |
+| `base64` | ✅ Yes       |
+
+## Use
+
+Create a `.js` file and copy the content from `script.js` to your project.
+
+In your HTML page, import the script:
+
+```html
+<script src="script.js" type="text/javascript">
+  CLIPBOARD.init('blob')
+</script>
+```
+
+For `Blob` mode, you have create a `<canvas>`:
+
+```html
+<canvas id="clipboard-canvas"></canvas>
+```
+
+### Listener
+
+Use `pasteFromClipboard` custom event to receive images pasted from clipboard.
+
+```js
+document.addEventListener('pasteFromClipboard', e => {
+  console.log('Image received', e.detail)
+})
+```
+
+The listener returns:
+
+```json
+{
+  "bubbles": false,
+  "cancelBubble": false,
+  ...
+  "detail": { ... }, // <-- returns pasted image
+  
+}
+```
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // inspired on http://ourcodeworld.com/articles/read/491/how-to-retrieve-images-from-the-clipboard-with-javascript-in-the-browser
 
 var CLIPBOARD = (function () {
-  let MODE;
+  let MODE
 
   /**
    * This handler retrieves the images from the clipboard as a blob and returns it in a callback.
@@ -9,32 +9,32 @@ var CLIPBOARD = (function () {
    * @param pasteEvent
    * @param callback
    */
-  const convertFromClipboardToBlob = (pasteEvent, callback) => {
+  function convertFromClipboardToBlob(pasteEvent, callback) {
     if (pasteEvent.clipboardData === false) {
-      if (typeof callback === "function") {
-        callback(undefined);
+      if (typeof callback === 'function') {
+        callback(undefined)
       }
     }
 
     const {
       clipboardData: { items }
-    } = pasteEvent;
+    } = pasteEvent
 
     if (!items || items === undefined) {
-      console.warn("Clipboard is empty");
-      if (typeof callback === "function") callback(undefined);
+      console.warn('Clipboard is empty')
+      if (typeof callback === 'function') callback(undefined)
     }
 
     for (let i in items) {
       if (items[i].type) {
-        if (items[i].type.indexOf("image") === -1) continue;
+        if (items[i].type.indexOf('image') === -1) continue
 
-        const blob = items[i].getAsFile();
+        const blob = items[i].getAsFile()
 
-        if (typeof callback === "function") callback(blob);
+        if (typeof callback === 'function') callback(blob)
       }
     }
-  };
+  }
 
   /**
    * This handler retrieves the images from the clipboard as a base64 string and returns it in a callback.
@@ -42,108 +42,116 @@ var CLIPBOARD = (function () {
    * @param pasteEvent
    * @param callback
    */
-  const retrieveImageFromClipboardAsBase64 = (
-    pasteEvent,
-    callback,
-    imageFormat
-  ) => {
+  function retrieveImageFromClipboardAsBase64(pasteEvent, callback, imageFormat) {
     if (pasteEvent.clipboardData === false) {
-      if (typeof callback === "function") {
-        callback(undefined);
+      if (typeof callback === 'function') {
+        callback(undefined)
       }
     }
 
     const {
       clipboardData: { items }
-    } = pasteEvent;
+    } = pasteEvent
 
     if (!items || items === undefined) {
-      console.warn("Clipboard is empty");
-      if (typeof callback === "function") callback(undefined);
+      console.warn('Clipboard is empty')
+      if (typeof callback === 'function') callback(undefined)
     }
 
     for (let i in items) {
       if (items[i].type) {
-        if (items[i].type.indexOf("image") === -1) continue;
+        if (items[i].type.indexOf('image') === -1) continue
 
         // Retrieve image on clipboard as blob
-        const blob = items[i].getAsFile();
+        const blob = items[i].getAsFile()
 
         // Create an abstract canvas and get context
-        var mycanvas = document.createElement("canvas");
-        var ctx = mycanvas.getContext("2d");
+        var mycanvas = document.createElement('canvas')
+        var ctx = mycanvas.getContext('2d')
 
         // Create an image
-        var img = new Image();
+        var img = new Image()
 
         // Once the image loads, render the img on the canvas
         img.onload = function () {
           // Update dimensions of the canvas with the dimensions of the image
-          mycanvas.width = this.width;
-          mycanvas.height = this.height;
+          mycanvas.width = this.width
+          mycanvas.height = this.height
 
           // Draw the image
-          ctx.drawImage(img, 0, 0);
+          ctx.drawImage(img, 0, 0)
 
           // Execute callback with the base64 URI of the image
-          if (typeof callback === "function") {
-            callback(mycanvas.toDataURL(imageFormat || "image/png"));
+          if (typeof callback === 'function') {
+            callback(mycanvas.toDataURL(imageFormat || 'image/png'))
           }
-        };
+        }
 
         // Crossbrowser support for URL
-        var URLObj = window.URL || window.webkitURL;
+        var URLObj = window.URL || window.webkitURL
 
         // Creates a DOMString containing a URL representing the object given in the parameter
         // namely the original Blob
-        img.src = URLObj.createObjectURL(blob);
+        img.src = URLObj.createObjectURL(blob)
       }
     }
-  };
+  }
 
-  const handleListener = (emit) => {
-    return (pasteEvent) => {
-      if (MODE === "blob") {
-        convertFromClipboardToBlob(pasteEvent, (imageBlob) => {
+  function handleListener(emit) {
+    return function (pasteEvent) {
+      if (MODE === 'blob') {
+        convertFromClipboardToBlob(pasteEvent, function (imageBlob) {
           if (imageBlob) {
-            const canvas = document.getElementById("my-image");
-            const ctx = canvas.getContext("2d");
+            const canvas = document.getElementById('clipboard-canvas')
+            const ctx = canvas.getContext('2d')
 
-            const img = new Image();
+            const img = new Image()
 
             img.onload = function () {
-              canvas.width = this.width;
-              canvas.height = this.height;
+              canvas.width = this.width
+              canvas.height = this.height
 
-              ctx.drawImage(img, 0, 0);
-            };
+              ctx.drawImage(img, 0, 0)
+            }
 
             // Crossbrowser support for URL
-            var URLObj = window.URL || window.webkitURL;
+            var URLObj = window.URL || window.webkitURL
 
             // Creates a DOMString containing a URL representing the object given in the parameter
             // namely the original Blob
-            img.src = URLObj.createObjectURL(imageBlob);
+            img.src = URLObj.createObjectURL(imageBlob)
 
-            document.dispatchEvent(emit(imageBlob));
+            document.dispatchEvent(emit(imageBlob))
           }
-        });
-      } else if (MODE === "base64") {
+        })
+      } else if (MODE === 'base64') {
         //     // Handle the event
-        retrieveImageFromClipboardAsBase64(pasteEvent, function (
-          imageDataBase64
-        ) {
+        retrieveImageFromClipboardAsBase64(pasteEvent, function (imageDataBase64) {
           // If there's an image, open it in the browser as a new window :)
           if (imageDataBase64) {
             // data:image/png;base64,iVBORw0KGgoAAAAN......
-            console.log(imageDataBase64);
+            console.log(imageDataBase64)
 
-            document.dispatchEvent(emit(imageDataBase64));
+            document.dispatchEvent(emit(imageDataBase64))
           }
-        });
+        })
       }
-    };
-  };
+    }
+  }
+
+  document.addEventListener(
+    'paste',
+    function (event) {
+      event.stopPropagation()
+
+      var emit = function (data) {
+        return new CustomEvent('pasteFromClipboard', { detail: data })
+      }
+
+      handleListener(emit)(event)
+    },
+    false
+  )
 
   return {
     /**
@@ -151,18 +159,7 @@ var CLIPBOARD = (function () {
      * @param mode "blob" | "base64"
      */
     init: function (mode) {
-      MODE = mode;
-
-      function emit(data) {
-        return new CustomEvent("pasteFromClipboard", { detail: data });
-      }
-
-      document.removeEventListener(
-        "paste",
-        () => console.log("Removed"),
-        false
-      );
-      document.addEventListener("paste", handleListener(emit), false);
+      MODE = mode
     }
-  };
-})();
+  }
+})()
